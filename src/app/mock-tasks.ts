e32b6e08416f2a543adb100b7c2c068e51aad134
Josh Colvin <joshuacolvin@Joshuas-MacBook-Pro-2.local>
2022-11-10 20:49:19 -0500

    Added Tasks and Task components with simple styling. Utilized Angular looping directive

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task';
+
+export const TASKS:Task[] = [
+	{
+		id: 1, 
+		text: "Doctors appointment",
+		day: "May 4 at 4:00pm",
+		reminder: true
+	},
+	{
+		id: 2, 
+		text: "Meeting at school",
+		day: "May 6th at 1:30pm",
+		reminder: true
+	},
+	{
+		id: 3, 
+		text: "Food shoping",
+		day: "May 7th at 12:30pm",
+		reminder: false
+	}
+];
